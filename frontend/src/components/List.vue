d2e97703fa9229171f99bda3d153673dd6c379cd
Unknown <paul.aboulinc@outlook.fr>
2018-01-09 16:23:44 +0100

    frontend init

---
+++ frontend/src/components/List.vue
@@ -0,0 +1,29 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Liste de todo</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <td v-for="todo in todos">
+        {{ todo }}
+      </td>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'List',
+
+  // Ici, les données de notre composant
+  data () {
+    return {
+      todos: []
+    }
+  }
+}
+</script>
